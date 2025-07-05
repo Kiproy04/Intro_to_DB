1fd191f202528f0c1e897ff574e499ab2fdeb44f
Leroy Kirui <delleroy04@gmail.com>
2025-07-05 18:23:23 +0300

    Made a connection

---
+++ MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+def connect_to_mysql():
+    try:
+        connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "leroymysql-1",
+            database = "alx_book_store",
+            port = 3306,
+
+        )
+        if connection.is_connected():
+            print("Database alx_book_store created successfully!")
+            return connection
+        
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+    
+def close_connection(connection):
+    if connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed.")
+
+        
