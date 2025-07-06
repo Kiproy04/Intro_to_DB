3b88e489c3f58a0734d04459b8d6cb32427e070b
Leroy Kirui <delleroy04@gmail.com>
2025-07-06 03:47:17 +0300

    Task 4 completed

--- task_4.sql
+++ task_4.sql
@@ -2,5 +2,4 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 SELECT "COLUMN_NAME", "COLUMN_TYPE"
 FROM "INFORMATION_SCHEMA.COLUMNS"
-WHERE "TABLE_SCHEMA" = 'alx_book_store'
-AND "TABLE_NAME" = 'Books';
+WHERE "TABLE_SCHEMA = 'alx_book_store'" AND "TABLE_NAME = 'Books'";
